0e9d08bddffccef28004a181e78f32cdc68dcd6c
Yılmaz ÇABUK <yilmazcabuk@protonmail.com>
2023-02-25 14:14:14 +0300

    feat: implement new Next.js metadata feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { ChildrenProps } from 'react';
+import type { Metadata } from 'next';
 
 import Footer from './Footer';
 import Header from './Header';
@@ -9,13 +10,19 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === 'true') {
   import('../../mocks');
 }
 
+export const metadata: Metadata = {
+  title: 'Next.js Tailwind Template',
+  themeColor: 'purple',
+  colorScheme: 'dark',
+  icons: {
+    icon: '/vercel.svg',
+    shortcut: '/favicon.ico',
+  },
+};
+
 export default function RootLayout({ children }: ChildrenProps) {
   return (
-    <html className="h-full" lang="en" data-theme="light">
-      <head>
-        <title>Next.js Tailwind Template</title>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
+    <html className="h-full" lang="en">
       <body className="flex h-full flex-col">
         <Header />
         <main className="flex-1">{children}</main>
